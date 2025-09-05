c3e8f9ea6d37269d89d669b6a7084a7be47fff56
dlsrb <dlsrb1997@naver.com>
2025-09-05 11:53:29 +0900

    test

--- 01 openweather/weather.js
+++ 01 openweather/weather.js
@@ -1,4 +1,4 @@
-url = "https://api.openweathermap.org/data/2.5/find?q=Seoul&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35"
+weatherurl = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35"
 
 function getweather(){
     // alert("getweather가 수행됨")
